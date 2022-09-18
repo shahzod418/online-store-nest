781abae16b1fcdbc07ce8619629b10ce715021c9
Davlatov Shahzod <shahzod@davlatov.space>
2022-09-18 20:26:32 +0300

    Start

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
+import { AppService } from './app.service';
+import { AppResolver } from './app.resolver';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<MercuriusDriverConfig>({
+      driver: MercuriusDriver,
+      autoSchemaFile: true,
+      graphiql: true,
+    }),
+  ],
+  providers: [AppResolver, AppService],
+})
+export class AppModule {}
